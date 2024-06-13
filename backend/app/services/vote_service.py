e70c60b524001f28d40940dbeaf880e8284a67e4
Julian Navarrete <julyplottier@hotmail.com>
2024-06-13 20:50:46 -0300

    fixed vote_service, now create_vote works

--- backend/app/services/vote_service.py
+++ backend/app/services/vote_service.py
@@ -1,3 +1,4 @@
+
 from app.schemas.vote_schema import VoteOut
 from app.models.candidate_model import CandidateModel
 from app.models.election_model import ElectionModel
@@ -13,20 +14,27 @@ from bson import ObjectId
 from pymongo.errors import DuplicateKeyError
 from beanie import Link
 import json
-from datetime import datetime as dt
+from datetime import datetime as dt, UTC
 
 
 class VoteService:
     @staticmethod
-    async def create_vote(dni: int,candidate_id: str, election_id: str) -> VoteModel:
+    async def create_vote(dni: int, candidate_id: str, election_id: str) -> VoteOut:
         try:
-            # object_id = ObjectId(dni)
+            existing_vote = await VoteModel.find_one(
+                VoteModel.voter.dni == dni,
+                VoteModel.election.id == election_id
+            )
+
+            if existing_vote:
+                raise HTTPException(status_code=400, detail="User has already voted in this election")
+
             user = await UserModel.find_one(UserModel.dni == dni)
             if not user:
                 raise HTTPException(status_code=404, detail="User not found")
 
             user = UserOutVote(
-                id=user.id,
+                id=str(user.id),
                 dni=user.dni,
                 email=user.email,
                 first_name=user.first_name,
@@ -39,20 +47,10 @@ class VoteService:
                 raise HTTPException(status_code=404, detail="Election not found")
             
             election = ElectionOutVote(
-                id=ObjectId(election_id),
+                id=election_id,
                 name=election.name,
                 description=election.description,
             )
-
-
-            existing_vote = await VoteModel.find_one(
-                VoteModel.voter == Link[UserModel](ObjectId(dni)) &
-                VoteModel.election == Link[ElectionModel](ObjectId(election_id))
-            )
-
-            if existing_vote:
-                raise HTTPException(status_code=400, detail="User has already voted in this election")
-
 
             object_id = ObjectId(candidate_id)
             candidate = await CandidateModel.find_one(CandidateModel.id == object_id)
@@ -60,46 +58,32 @@ class VoteService:
                 raise HTTPException(status_code=404, detail="Candidate not found")
 
             candidate = CandidateOutVote(
-                id=ObjectId(candidate_id),
+                id=candidate_id,
                 name=candidate.name,
                 party=candidate.party,
             )
 
             # Create vote
             vote = VoteModel(
-                voter=Link[UserModel](dni),
-                candidate=Link[CandidateModel](candidate_id),
-                election=Link[ElectionModel](election_id),
-                timestamp=dt.now(dt.timezone.utc)
+                voter=user.model_dump(),
+                candidate=candidate.model_dump(),
+                election=election.model_dump(),
+                timestamp=dt.now(UTC)
             )
 
-            # Guardar el voto en la base de datos
+            # Save vote to database
             await vote.save()
-            return vote
 
+            vote_out = VoteOut(
+                id=str(vote.id),
+                voter=str(vote.voter["id"]), 
+                candidate=str(vote.candidate["id"]), 
+                election=str(vote.election["id"]), 
+                timestamp=vote.timestamp
+            )
+            return vote_out
 
-
-
-            '''
-
-            if all(link.ref.id != user.id for link in user.id):
-                vote.voter.add(user)
-            else:
-                raise HTTPException(status_code=400, detail="Election already exists in the candidate's list")
-
-            if all(link.ref.id != election.id for link in candidate.elections):
-                candidate.elections.append(election)
-            else:
-                raise HTTPException(status_code=400, detail="Election already exists in the candidate's list")
-            
-
-
-            await vote.save()
-            # candidate.id = str(candidate.id)
-            return candidate
-
-            '''
-            
+    
         except HTTPException as http_exc:
             # Re-raise HTTPException to avoid capturing it as a 500 error
             raise http_exc
@@ -111,12 +95,6 @@ class VoteService:
             )
 
 
-
-
-
-
-
-
     @staticmethod
     async def get_vote_by_id(id: str) -> Optional[VoteOut]:
         object_id = ObjectId(id)
@@ -126,45 +104,9 @@ class VoteService:
 
         return VoteOut(
             id=str(vote.id),
-            voter=vote.voter,
-            candidate=vote.candidate,
-            election=vote.election,
+            voter=str(vote.voter.id), 
+            candidate=str(vote.candidate.id), 
+            election=str(vote.election.id), 
             timestamp=vote.timestamp
         )
 
-
-'''
-
-    @staticmethod
-    async def update_candidate(id: str, data: CandidateUpdate) -> CandidateOut:
-        object_id = ObjectId(id)
-        candidate = await CandidateModel.find_one(CandidateModel.id == object_id)
-        if not candidate:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Candidate not found"
-            )
-
-        update_data = data.model_dump(exclude_unset=True)
-        for key, value in update_data.items():
-            setattr(candidate, key, value)
-        await candidate.save()
-
-        return CandidateOut(
-            id=str(candidate.id),
-            name=candidate.name,
-            party=candidate.party,
-            bio=candidate.bio,
-            election_id=candidate.elections
-        )
-
-
-    @staticmethod
-    async def delete_candidate(id: str) -> None:
-        object_id = ObjectId(id)
-        candidate = await CandidateModel.find_one(CandidateModel.id == object_id)
-        if not candidate:
-            raise HTTPException(status_code=404, detail="Candidate not found")
-        await candidate.delete()
-
-'''
